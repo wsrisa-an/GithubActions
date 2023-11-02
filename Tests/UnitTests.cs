fb2963c03bf3a427f285363e7b82228f69ecb033
wsrisa-an <89483793+wsrisa-an@users.noreply.github.com>
2023-11-02 17:10:19 -0500

    Update UnitTests.cs

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -34,7 +34,7 @@ namespace GithubActionsLab
         [Test]
         public void Subtract_Valid()
         {
-            Assert.AreEqual(2, Program.Subtract("2", "1"));
+            Assert.AreEqual(1, Program.Subtract("2", "1"));
             Assert.AreEqual(1, Program.Subtract("3", "2"));
             Assert.AreEqual(2, Program.Subtract("7", "5"));
         }
